9f5ca983973f0d8e1c389fef69133804f0d89d3c
gmanandmarbles <119905567+gmanandmarbles@users.noreply.github.com>
2026-02-07 13:58:41 -0700

    Add shooter/intake constants and controls
    Introduce ShooterConstants and IntakeConstants (CAN IDs, PID/FF values, RPM settings) and update OperatorConstants port. Replace example wiring with a TestingSubsystem: configure SparkMax devices for intake, shooter, and kicker; add encoder, PIDController and SimpleMotorFeedforward for closed-loop shooter control; expose command factories for shooter, intake, and kicker. Wire controller bindings in RobotContainer (Y: shooter, bumpers: intake/kicker, D-pad: RPM adjustments). Remove example subsystem/commands and references.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,19 +1,28 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
+    public static class OperatorConstants {
+        public static final int kDriverControllerPort = 1;
+    }
+
+    public static class ShooterConstants {
+        public static final int SHOOTER_LEADER_CANID = 14;
+        public static final int SHOOTER_FOLLOWER_CANID = 15;
+        public static final int KICKER_CANID = 16; 
+
+        // PIDF Values
+        public static final double LEADER_Kp = 0.01;
+        public static final double LEADER_FF_kS = 0.1;
+        public static final double LEADER_FF_kV = 0.4;
+        
+        // Settings
+        public static final double RPM_INCREMENT = 10.0;
+        public static final double DEFAULT_TARGET_RPM = 2000.0;
+    }
+
+    public static class IntakeConstants {
+        public static final int INTAKE_LEADER_ID = 10;
+        public static final int INTAKE_FOLLOWER_ID = 11;
+        public static final double INTAKE_SPEED = 0.7;
+    }
 }
--- src/main/java/frc/robot/subsystems/TestingSubsystem.java
+++ src/main/java/frc/robot/subsystems/TestingSubsystem.java
@@ -1,81 +1,90 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.spark.SparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ShooterConstants;
+import frc.robot.Constants.IntakeConstants;
+import frc.robot.Configs.ShooterConfig; // Assuming ShooterConfig handles the 14/15/16 setup
+import frc.robot.Configs.IntakeConfigs;  // Assuming IntakeConfigs handles the 10/11 setup
+
 public class TestingSubsystem extends SubsystemBase {
-  // --- Hardware Definitions ---
-  private final SparkMax m_intakeLeader = new SparkMax(10, MotorType.kBrushless);
-  private final SparkMax m_intakeFollower = new SparkMax(11, MotorType.kBrushless);
-  private final SparkMax m_shooterLeft = new SparkMax(14, MotorType.kBrushless);
-  private final SparkMax m_shooterRight = new SparkMax(15, MotorType.kBrushless);
-  private final SparkMax m_kicker = new SparkMax(16, MotorType.kBrushless);
+    // Hardware
+    private final SparkMax m_intakeLeader = new SparkMax(IntakeConstants.INTAKE_LEADER_ID, MotorType.kBrushless);
+    private final SparkMax m_intakeFollower = new SparkMax(IntakeConstants.INTAKE_FOLLOWER_ID, MotorType.kBrushless);
+    private final SparkMax m_shooterLeader = new SparkMax(ShooterConstants.SHOOTER_LEADER_CANID, MotorType.kBrushless);
+    private final SparkMax m_shooterFollower = new SparkMax(ShooterConstants.SHOOTER_FOLLOWER_CANID, MotorType.kBrushless);
+    private final SparkMax m_kicker = new SparkMax(ShooterConstants.KICKER_CANID, MotorType.kBrushless);
 
-  /** Creates a new TestingSubsystem. */
-  public TestingSubsystem() {
-    configureHardware();
-  }
+    // Control Objects
+    private final RelativeEncoder m_shooterEncoder;
+    private final PIDController m_shooterPID;
+    private final SimpleMotorFeedforward m_shooterFF;
 
-  private void configureHardware() {
-    SparkMaxConfig baseConfig = new SparkMaxConfig();
-    baseConfig.smartCurrentLimit(40); 
+    // State Variables
+    private double m_targetRPM = ShooterConstants.DEFAULT_TARGET_RPM;
 
-    SparkMaxConfig intakeFollowerConfig = new SparkMaxConfig();
-    intakeFollowerConfig.follow(10, true);
+    public TestingSubsystem() {
+        // Configure Intake
+        m_intakeLeader.configure(IntakeConfigs.intakeLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        m_intakeFollower.configure(IntakeConfigs.intakeFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        
+        // Configure Shooter/Kicker
+        m_shooterLeader.configure(ShooterConfig.shooterLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        m_shooterFollower.configure(ShooterConfig.shooterFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        m_kicker.configure(ShooterConfig.shooterFeederConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
-    SparkMaxConfig shooterFollowerConfig = new SparkMaxConfig();
-    shooterFollowerConfig.follow(14, true);
+        m_shooterEncoder = m_shooterLeader.getEncoder();
+        m_shooterPID = new PIDController(ShooterConstants.LEADER_Kp, 0, 0);
+        m_shooterFF = new SimpleMotorFeedforward(ShooterConstants.LEADER_FF_kS, ShooterConstants.LEADER_FF_kV);
+    }
 
-    SparkMaxConfig kickerConfig = new SparkMaxConfig();
-    kickerConfig.inverted(true);
+    // --- Action Methods ---
 
-    // Apply configurations
-    m_intakeLeader.configure(baseConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    m_intakeFollower.configure(intakeFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    m_shooterLeft.configure(baseConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    m_shooterRight.configure(shooterFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    m_kicker.configure(kickerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-  }
+    public void runShooterAtTarget() {
+        double pidOutput = m_shooterPID.calculate(m_shooterEncoder.getVelocity(), m_targetRPM);
+        double ffOutput = m_shooterFF.calculate(m_targetRPM / 60.0);
+        m_shooterLeader.set(MathUtil.clamp(pidOutput + ffOutput, -1.0, 1.0));
+    }
 
-  // --- Methods (Actions) ---
+    public void setIntake(double speed) { m_intakeLeader.set(speed); }
+    public void setKicker(double speed) { m_kicker.set(speed); }
+    
+    public void incrementRPM() { m_targetRPM += ShooterConstants.RPM_INCREMENT; }
+    public void decrementRPM() { m_targetRPM -= ShooterConstants.RPM_INCREMENT; }
 
-  public void setIntakeSpeed(double speed) {
-    m_intakeLeader.set(speed);
-  }
+    public void stopAll() {
+        m_intakeLeader.set(0);
+        m_shooterLeader.set(0);
+        m_kicker.set(0);
+    }
 
-  public void setShooterSpeed(double speed) {
-    m_shooterLeft.set(speed);
-  }
+    // --- Command Factories ---
 
-  public void setKickerSpeed(double speed) {
-    m_kicker.set(speed);
-  }
+    public Command runShooterCommand() {
+        return run(this::runShooterAtTarget);
+    }
 
-  public void stopAll() {
-    m_intakeLeader.set(0);
-    m_shooterLeft.set(0);
-    m_kicker.set(0);
-  }
+    public Command runIntakeCommand() {
+        return startEnd(() -> setIntake(IntakeConstants.INTAKE_SPEED), () -> setIntake(0));
+    }
 
-  // --- Command Factories ---
+    public Command runKickerCommand(double speed) {
+        return startEnd(() -> setKicker(speed), () -> setKicker(0));
+    }
 
-  /** Simple command to run the kicker while held. */
-  public Command runKickerCommand(double speed) {
-    return startEnd(() -> setKickerSpeed(speed), () -> setKickerSpeed(0));
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run. 
-    // Great for putting data to SmartDashboard.
-  }
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Testing/Target RPM", m_targetRPM);
+        SmartDashboard.putNumber("Testing/Actual RPM", m_shooterEncoder.getVelocity());
+    }
 }
