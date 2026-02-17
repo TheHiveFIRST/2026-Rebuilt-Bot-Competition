0acc2e079e79d175e73fefff6e024d8f63a32f52
supersoimon <simon.gidrewicz@gmail.com>
2026-02-17 14:30:39 -0700

    funny

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -24,9 +24,9 @@ public final class Constants {
     public static final double MAX_ANGULAR_SPEED =  2*Math.PI; // rad/s 
     //Chassis config - width, depth, CAN IDS and angular offset values in Designdoc.md
   
-    public static final double WHEEL_CENTER_WIDTH = Units.inchesToMeters(11.75);
+    public static final double WHEEL_CENTER_WIDTH = Units.inchesToMeters(22.8);
     // Distance between centers of right and left wheels on robot
-    public static final double WHEEL_CENTER_DEPTH = Units.inchesToMeters(11.75);
+    public static final double WHEEL_CENTER_DEPTH = Units.inchesToMeters(22.8);
     // Distance between front and back wheels on robot
     //depth/frontback distance from robot center to each wheel 
 
