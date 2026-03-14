781960e7b6a423968d030bfdb707308624f50fef
supersoimon <simon.gidrewicz@gmail.com>
2026-03-14 14:26:08 -0600

    so turning works and moving works but both dont sadly :(

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import javax.net.ssl.TrustManagerFactory;
+
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 //import edu.wpi.first.math.trajectory.TrapezoidProfile;
