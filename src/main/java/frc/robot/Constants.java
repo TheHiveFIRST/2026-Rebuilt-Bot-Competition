3184d289fbac2f9976b362cf94f28453e97acdc6
MOoshalll <Mbkturner@gmail.com>
2026-03-15 13:52:01 -0600

    changed intake pivot setpoints to postitions accurate as of noon on march 15

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -186,9 +186,10 @@ public final class Constants {
         public static final double ARM_KI = 0.0;
         public static final double ARM_KD = 0.0;
 
-        public static final double PIVOT_OUT = 0.57; 
-        public static final double PIVOT_DEFAULT = 0.57;// was at 0.5 before 
-        public static final double PIVOT_OUT_2 = 0.53; 
+        public static final double PIVOT_OUT = 0.59; 
+        public static final double PIVOT_DEFAULT = 0.59;// was at 0.5 before 
+        public static final double PIVOT_OUT_2 = 0.53; //why do we have this
+        public static final double PIVOT_WOBBLE = 0.53;// was at 0.5 before 
 
         public static final double PIVOT_IN = 0.184;  
         public static final double ARM_KP_INCREMENT = 0.01;
