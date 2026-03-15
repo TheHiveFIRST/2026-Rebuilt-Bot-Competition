6cd54d903cb2834daab62ef314274cec9ff629a8
MOoshalll <Mbkturner@gmail.com>
2026-03-15 13:21:50 -0600

    hi:

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -186,8 +186,8 @@ public final class Constants {
         public static final double ARM_KI = 0.0;
         public static final double ARM_KD = 0.0;
 
-        public static final double PIVOT_OUT = 0.7; 
-        public static final double PIVOT_DEFAULT = 0.615;// was at 0.5 before 
+        public static final double PIVOT_OUT = 0.57; 
+        public static final double PIVOT_DEFAULT = 0.57;// was at 0.5 before 
         public static final double PIVOT_OUT_2 = 0.53; 
 
         public static final double PIVOT_IN = 0.184;  
