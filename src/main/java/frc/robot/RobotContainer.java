822f8dc540897f32a1b4124324d049d1e992807a
supersoimon <simon.gidrewicz@gmail.com>
2026-03-15 17:50:46 -0600

    changed path

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -215,7 +215,7 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return new PathPlannerAuto("Turn.90");
+    return new PathPlannerAuto("HumanPlayerMiddleAuto");
 
     //return autoChooser.getSelected();
   }
