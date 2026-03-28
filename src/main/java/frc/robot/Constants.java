2cadaa4ccdcea9c95493811f711737568b51a27d
supersoimon <simon.gidrewicz@gmail.com>
2026-03-28 14:30:01 -0600

    undid power changes, moved robot further towards hub (to account)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -242,7 +242,7 @@ public final class Constants {
 
         public static final double VELOCITY_TOLERANCE =  30; 
 
-        public static final double HUB_TARGET_RPM = 4480; //TUNED 
+        public static final double HUB_TARGET_RPM = 4360; //TUNED 
         public static final double TRENCH_TARGET_RPM = 6800; 
         public static final double LADDER_TARGET_RPM = 6300;
         public static final double PASSING_TARGET_RPM = 8000;
