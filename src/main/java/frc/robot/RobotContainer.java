09e5723cf04a6d32855f6107ddb84cd0a68033dc
supersoimon <simon.gidrewicz@gmail.com>
2026-04-05 21:20:21 -0600

    added oldintakewobble (old intake wobble command), redid intake wobble so that it spins intake when above position 0.45, and agitates to a lower position every time

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -228,7 +228,7 @@ public class RobotContainer {
     
     public Command shootWithAgitation(){
       return Commands.parallel(           
-      new IntakeWobbleCommand(mArmSubsystem).repeatedly(),
+      new IntakeWobbleCommand(mArmSubsystem, mIntakeSubsystem).repeatedly(),
       mShooterSubsystem.runKickerCommand());
       
     }
