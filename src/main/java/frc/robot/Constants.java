c36ed9086d512a1f51d85464226a98bbd91493c8
supersoimon <simon.gidrewicz@gmail.com>
2026-04-12 18:04:59 -0600

    updated shot power

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -241,7 +241,7 @@ public final class Constants {
 
         public static final double VELOCITY_TOLERANCE =  30; 
 
-        public static final double HUB_TARGET_RPM = 6300; //TUNED //not 4440
+        public static final double HUB_TARGET_RPM = 6240; //TUNED //not 4440
         public static final double TRENCH_TARGET_RPM = 6800; 
         public static final double LADDER_TARGET_RPM = 6300;
                 public static final double AUTORPM = 5700;
