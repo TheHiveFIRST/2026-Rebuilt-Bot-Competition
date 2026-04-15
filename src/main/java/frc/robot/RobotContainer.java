09ef0b5198ad75cb7af8ac8bf47ce90756d5340a
MOoshalll <Mbkturner@gmail.com>
2026-04-15 14:02:12 -0600

    changed the binding for toggleDistanceEstimation from a whileTrue to an Ontrue. hopefully this fixes the issue with it being inconsistent

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -140,7 +140,7 @@ public class RobotContainer {
         mOperatorController.rightTrigger().onTrue(mShooterSubsystem.setPassingShotCommand());
         mOperatorController.b().onTrue(mShooterSubsystem.setHubShotCommand());
         mOperatorController.a().onTrue(mShooterSubsystem.setTrenchShotCommand());
-        mOperatorController.x().whileTrue(mShooterSubsystem.toggleDistanceEstimationCommand());
+        mOperatorController.x().onTrue(mShooterSubsystem.toggleDistanceEstimationCommand());
         mOperatorController.rightBumper().onTrue(mShooterSubsystem.increaseShootingRPMOffsetCommand());
         mOperatorController.leftBumper().onTrue(mShooterSubsystem.decreaseShootingRPMOffsetCommand());
         //mOperatorController.povLeft().onTrue(mShooterSubsystem.runOnce(mShooterSubsystem::incrementRPM));
